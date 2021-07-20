b00914a34e936e8ad05211b88d93ad0ffb6acf46
sergiarguimbau <sergiarguimbau@gmail.com>
2021-07-20 23:12:44 +0200

    Change rules for 5x5 size

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-const X_WIN = "♟♟♟";
-const O_WIN = "♙♙♙";
-
 export const BOARD_SIZE = 5
 const steps = [...Array(BOARD_SIZE).keys()];
+
+const X_WIN = "♟".repeat(BOARD_SIZE);
+const O_WIN = "♙".repeat(BOARD_SIZE);
 
 const checkValue = value => {
   if (value === X_WIN) {
@@ -16,7 +16,7 @@ export const getGameStatus = moves => {
   let gameStatus;
   // Check horizontal
   steps.find((step, i) => {
-    gameStatus = checkValue(`${moves[step * 3]}${moves[step * 3 + 1]}${moves[step * 3 + 2]}`);
+    gameStatus = checkValue(`${moves[step * BOARD_SIZE + 0]}${moves[step * BOARD_SIZE + 1]}${moves[step * BOARD_SIZE + 2]}${moves[step * BOARD_SIZE + 3]}${moves[step * BOARD_SIZE + 4]}`);
     return gameStatus !== undefined;
   });
 
@@ -24,17 +24,17 @@ export const getGameStatus = moves => {
 
   // Check vertical
   steps.find(row => {
-    gameStatus = checkValue(`${moves[row]}${moves[row + 3]}${moves[row + 6]}`);
+    gameStatus = checkValue(`${moves[row + BOARD_SIZE * 0]}${moves[row + BOARD_SIZE * 1]}${moves[row + BOARD_SIZE * 2]}${moves[row + BOARD_SIZE * 3]}${moves[row + BOARD_SIZE * 4]}`);
     return gameStatus !== undefined;
   });
 
   if (gameStatus) return gameStatus;
 
   // Check diagonal
-  gameStatus = checkValue(`${moves[0]}${moves[4]}${moves[8]}`);
+  gameStatus = checkValue(`${moves[0 + BOARD_SIZE * 0]}${moves[1 + BOARD_SIZE * 1]}${moves[2 + BOARD_SIZE * 2]}${moves[3 + BOARD_SIZE * 3]}${moves[4 + BOARD_SIZE * 4]}`);
   if (gameStatus) return gameStatus;
 
-  gameStatus = checkValue(`${moves[2]}${moves[4]}${moves[6]}`);
+  gameStatus = checkValue(`${moves[(BOARD_SIZE - 1) * 1]}${moves[(BOARD_SIZE - 1) * 2]}${moves[(BOARD_SIZE - 1) * 3]}${moves[(BOARD_SIZE - 1) * 4]}${moves[(BOARD_SIZE - 1) * 5]}`);
   if (gameStatus) return gameStatus;
 
   if (Object.values(moves).length === BOARD_SIZE*BOARD_SIZE && !gameStatus) return "DRAW";
